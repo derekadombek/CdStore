b328c327056af8be9d1bbc5f250acdd0c633cba2
DDombek@swdv.local <DDombek@swdv.local>
2020-05-01 16:03:09 -0600

    added the db and validation

--- CdStore/Borrow.aspx.cs
+++ CdStore/Borrow.aspx.cs
@@ -11,36 +11,7 @@ namespace CdStore
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //current date on checkout and metal radio button checked on load
-            txtCheckoutDate.Text = DateTime.Today.ToShortDateString();
-            rdoMetal.Checked = true;
-        }
 
-        protected void btnSubmit_Click(object sender, EventArgs e)
-        {
-            
-            //success message after user submits
-            lblMessage.Text = "Great choice " + txtFirstName.Text + "! Have fun jamming out!";
-            
-        }
-
-        protected void btnClear_Click(object sender, EventArgs e)
-        {
-            //clears all values
-            txtCheckoutDate.Text = DateTime.Today.ToShortDateString();
-            txtReturnDate.Text = "";
-            ddlNoOfCD.SelectedIndex = -1;
-            rdoMetal.Checked = true;
-            rdoPunk.Checked = false;
-            rdoRap.Checked = false;
-            rdoCountry.Checked = false;
-            rdoRock.Checked = false;
-            txtFirstName.Text = "";
-            txtLastName.Text = "";
-            txtEmail.Text = "";
-            txtPhone.Text = "";
-            ddlPreferredMethod.SelectedIndex = 0;
-            lblMessage.Text = "";
         }
     }
 }
